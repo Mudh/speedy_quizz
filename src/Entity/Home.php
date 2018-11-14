c76ab4af233cdcb49742f4e1632a8d9f89b8a5f1
Eddy <vellaeddy@yahoo.fr>
2018-11-14 11:21:09 +0100

    Database initialisation + theme and home entity

---
+++ src/Entity/Home.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\HomeRepository")
+ */
+class Home
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $presentation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPresentation(): ?string
+    {
+        return $this->presentation;
+    }
+
+    public function setPresentation(string $presentation): self
+    {
+        $this->presentation = $presentation;
+
+        return $this;
+    }
+}
