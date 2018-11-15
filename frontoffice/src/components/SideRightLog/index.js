23fcf28207ebefcffd011a32c95c9b3a3616d1ee
Flo68 <flo7_126@hotmail.com>
2018-11-15 14:40:29 +0100

    maj syntaxe scss Navbar+Sidebar

--- frontoffice/src/components/SideRightLog/index.js
+++ frontoffice/src/components/SideRightLog/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './SideRightLog.scss';
 
 const SideRightLog = props => (
-  <div className="SideRightLog__container">
-    <h2>Bonjour Marco</h2>
-    <p> (logoengrenage) nb partie/s</p>
-    <p className="SideRightLog__container__p1"> (logoetoile) nb partie/s</p>
-    <p> (logoporte) nb Skip</p>
-    <p> (logocoeur) nb Revive</p>
-    <p> (logochrono) nb Timer</p>
-    <p className="SideRightLog__container__p1"> (logoreglage) nb 50/50</p>
-    <div className="SideRightLog__button">
-    <button type="button">New Game</button>
-  </div>
+  <div className="sideRightLog">
+    <div className="sideRightLog__Top">
+      <h2>Bonjour Marco</h2>
+      <p> (logoengrenage) nb partie/s</p>
+      <p className="sideRightLog__pfinish"> (logoetoile) nb partie/s</p>
+      <p> (logoporte) nb Skip</p>
+      <p> (logocoeur) nb Revive</p>
+      <p> (logochrono) nb Timer</p>
+      <p className="sideRightLog__pfinish"> (logoreglage) nb 50/50</p>
+    </div>
+    <div className="sideRightLog__button">
+      <button type="button">New Game</button>
+    </div>
   </div>
 
 );
