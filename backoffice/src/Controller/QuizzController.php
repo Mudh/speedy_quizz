d9bd4c2e41d17df23794a002e3f90646bdb884f4
Eddy <vellaeddy@yahoo.fr>
2018-11-15 19:10:03 +0100

    Json test encodage ok

--- backoffice/src/Controller/QuizzController.php
+++ backoffice/src/Controller/QuizzController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\ThemeRepository;
+use JMS\Serializer\SerializerBuilder;
+use App\Repository\QuestionRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -16,5 +19,26 @@ class QuizzController extends AbstractController
         $themeList = $themeRepo->findAll();
     
         return $this->json($themeList);
+    }
+
+    /**
+     * @Route("/quizz/test", name="quizz_test_list")
+     */
+    public function test(QuestionRepository $questionRepo)
+    {
+        $questions = $questionRepo->findAll(); 
+
+        foreach ($questions as $question) {
+            $question->setTitle(htmlentities($question->getTitle()));
+            
+            foreach($question->getResponse() as $response) {
+                $response->setResponse(htmlentities($response->getResponse()));
+            }
+        }
+
+        $serializer = SerializerBuilder::create()->build();
+        $jsonContent = $serializer->serialize($questions, 'json');
+    
+        return new Response($jsonContent);
     }
 }
