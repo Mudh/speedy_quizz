28ba1c5a7ca59a9fab4e05746511bf5dadac0d09
Mudh <jonmudh@gmail.com>
2018-11-15 15:24:03 +0100

    Create layout + home + style

---
+++ frontoffice/src/components/Home/index.js
@@ -0,0 +1,26 @@
+/**
+ * NPM import
+ */
+import React from 'react';
+
+/**
+ * Local import
+ */
+// Components
+import Layout from '../Layout';
+
+// Styles
+import './home.scss';
+
+/**
+ * Code
+ */
+const Home = () => (
+  <div id="home">
+    <Layout />
+  </div>
+);
+/**
+ * Export
+ */
+export default Home;
---
+++ frontoffice/src/components/Layout/index.js
@@ -0,0 +1,21 @@
+/**
+ * NPM import
+ */
+import React from 'react';
+
+/**
+ * Local import
+ */
+// Components
+
+// Styles
+import './layout.scss';
+
+/**
+ * Code
+ */
+const Layout = () => <main id="layout" />;
+/**
+ * Export
+ */
+export default Layout;
