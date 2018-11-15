1c18314f16be66474064c55ea2f922b01bacbcde
Flo68 <flo7_126@hotmail.com>
2018-11-15 15:08:34 +0100

    ajout icone profil + mis dans l'ordre les icones

--- frontoffice/src/components/Nav/index.js
+++ frontoffice/src/components/Nav/index.js
@@ -5,6 +5,7 @@ import Home from 'src/components/Icons/nav/home';
 import Infos from 'src/components/Icons/nav/infos';
 import Podium from 'src/components/Icons/nav/podium';
 import Socials from 'src/components/Icons/nav/socials';
+import Profil from 'src/components/Icons/nav/profil';
 import 'src/components/SideDrawer/DrawerToggleButton.scss';
 import './nav.scss';
 import 'src/components/Icons/icons.scss';
@@ -14,16 +15,17 @@ const Nav = props => (
     <nav className="toolbar__navigation">
       <div className="toolbar__navigation-items">
         <ul>
-          <li><a href="#"><Disconnect /></a></li>
-          <li><a href="#"><Faq /></a></li>
           <li><a href="#"><Home /></a></li>
-          <li><a href="#"><Infos /></a></li>
+          <li><a href="#"><Podium /></a></li>
+          <li><a href="#"><Faq /></a></li>
+          <li><a href="#"><Profil /></a></li>
+          <li><a href="#"><Disconnect /></a></li>
         </ul>
       </div>
       <div className="toolbar__navigation-items">
         <ul>
-          <li><a href="#"><Podium /></a></li>
           <li><a href="#"><Socials /></a></li>
+          <li><a href="#"><Infos /></a></li>
         </ul>
       </div>
     </nav>
