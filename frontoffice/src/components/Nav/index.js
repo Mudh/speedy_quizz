923dac4a804056c64af2f343d5dc58c17fd9c2da
Flo68 <flo7_126@hotmail.com>
2018-11-15 14:55:52 +0100

    intégrations des icones dans la nav

--- frontoffice/src/components/Nav/index.js
+++ frontoffice/src/components/Nav/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import Disconnect from 'src/components/Icons/nav/disconnect';
+import Faq from 'src/components/Icons/nav/faq';
+import Home from 'src/components/Icons/nav/home';
+import Infos from 'src/components/Icons/nav/infos';
+import Podium from 'src/components/Icons/nav/podium';
+import Socials from 'src/components/Icons/nav/socials';
 import 'src/components/SideDrawer/DrawerToggleButton.scss';
-
 import './nav.scss';
+import 'src/components/Icons/icons.scss';
 
 const Nav = props => (
   <header className="toolbar">
     <nav className="toolbar__navigation">
       <div className="toolbar__navigation-items">
         <ul>
-          <li><a href="#">Logo1</a></li>
-          <li><a href="#">Logo2</a></li>
-          <li><a href="#">Logo3</a></li>
-          <li><a href="#">Logo4</a></li>
+          <li><a href="#"><Disconnect /></a></li>
+          <li><a href="#"><Faq /></a></li>
+          <li><a href="#"><Home /></a></li>
+          <li><a href="#"><Infos /></a></li>
         </ul>
       </div>
       <div className="toolbar__navigation-items">
         <ul>
-          <li><a href="#">Logo5</a></li>
-          <li><a href="#">Logo6</a></li>
+          <li><a href="#"><Podium /></a></li>
+          <li><a href="#"><Socials /></a></li>
         </ul>
       </div>
     </nav>
