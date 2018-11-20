fd2c1242eedb5b61c73cc7b88a91582b3773ee74
Mudh <jonmudh@gmail.com>
2018-11-20 17:15:01 +0100

    Really solve conflicts it was close

--- backoffice/src/Utils/AccentEncoder.php
+++ backoffice/src/Utils/AccentEncoder.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 
 namespace App\Utils;
@@ -62,41 +61,4 @@ class AccentEncoder {
 
         return true;
     }
-=======
-<?php
-
-namespace App\Utils;
-
-class AccentEncoder {
-
-    public function getQuestionsAccents($questionsStepOne, $questionsStepTwo, $questionsStepThree) {
-
-        foreach ($questionsStepOne as $question) {
-            $question->setTitle(htmlentities($question->getTitle()));
-        
-            foreach($question->getResponse() as $response) {
-                $response->setResponse(htmlentities($response->getResponse()));
-            }
-        }
-
-        foreach ($questionsStepTwo as $question) {
-            $question->setTitle(htmlentities($question->getTitle()));
-
-            foreach($question->getResponse() as $response) {
-
-                $response->setResponse(htmlentities($response->getResponse()));
-            }
-        }
-
-        foreach ($questionsStepThree as $question) {
-            $question->setTitle(htmlentities($question->getTitle()));
-
-            foreach($question->getResponse() as $response) {
-                $response->setResponse(htmlentities($response->getResponse()));
-            }
-        }
-
-        return true;
-    }
->>>>>>> Stashed changes
 }
