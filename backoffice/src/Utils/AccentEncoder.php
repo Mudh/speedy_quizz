2b512c4ac6140b8f796a063956a4692a734a859d
Mudh <jonmudh@gmail.com>
2018-11-20 17:08:55 +0100

    Solve merge conflict

--- backoffice/src/Utils/AccentEncoder.php
+++ backoffice/src/Utils/AccentEncoder.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 
 namespace App\Utils;
@@ -61,4 +62,41 @@ class AccentEncoder {
 
         return true;
     }
+=======
+<?php
+
+namespace App\Utils;
+
+class AccentEncoder {
+
+    public function getQuestionsAccents($questionsStepOne, $questionsStepTwo, $questionsStepThree) {
+
+        foreach ($questionsStepOne as $question) {
+            $question->setTitle(htmlentities($question->getTitle()));
+        
+            foreach($question->getResponse() as $response) {
+                $response->setResponse(htmlentities($response->getResponse()));
+            }
+        }
+
+        foreach ($questionsStepTwo as $question) {
+            $question->setTitle(htmlentities($question->getTitle()));
+
+            foreach($question->getResponse() as $response) {
+
+                $response->setResponse(htmlentities($response->getResponse()));
+            }
+        }
+
+        foreach ($questionsStepThree as $question) {
+            $question->setTitle(htmlentities($question->getTitle()));
+
+            foreach($question->getResponse() as $response) {
+                $response->setResponse(htmlentities($response->getResponse()));
+            }
+        }
+
+        return true;
+    }
+>>>>>>> Stashed changes
 }
