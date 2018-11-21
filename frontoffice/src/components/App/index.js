7480ece6bd599932944322e5314f22afe07ab74d
Mudh <jonmudh@gmail.com>
2018-11-21 14:23:41 +0100

    NavLink + main routes

--- frontoffice/src/components/App/index.js
+++ frontoffice/src/components/App/index.js
@@ -2,12 +2,19 @@
  * NPM import
  */
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter,
+} from 'react-router-dom';
 /**
  * Local import
  */
 // Components
 import Nav from '../Nav';
+import Classement from '../Classement';
 import Quiz from '../../containers/Quiz';
 import SideRightLog from '../../containers/SideRightLog';
 import HomeVisitor from '../../containers/HomeVisitor';
@@ -20,11 +27,20 @@ import './app.scss';
  */
 const App = () => (
   <div id="app">
-    <Nav />
-    <HomeVisitor />
-    {/* <HomeMembre /> */}
-    {/* <Quiz /> */}
-    <SideRightLog />
+    <Router>
+      <div>
+        <Nav />
+
+        <Route exact path="/" component={HomeVisitor} />
+        <Route exact path="/classement" component={Classement} />
+        <Route exact path="/faq" render={() => <div>FAQ</div>} />
+        <Route exact path="/profil" render={() => <div>Profil</div>} />
+
+        {/* <HomeMembre /> */}
+        {/* <Quiz /> */}
+        {/* <SideRightLog /> */}
+      </div>
+    </Router>
   </div>
 );
 /**
