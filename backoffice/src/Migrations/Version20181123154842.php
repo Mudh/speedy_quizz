704e6bb6f4c8a7e1bd372b56bb488ed349acd03f
Mudh <jonmudh@gmail.com>
2018-11-23 17:53:34 +0100

    Set up env + migration

---
+++ backoffice/src/Migrations/Version20181123154842.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181123154842 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE coeff (id INT AUTO_INCREMENT NOT NULL, step_id INT DEFAULT NULL, level_id INT DEFAULT NULL, coeff DOUBLE PRECISION NOT NULL, alias VARCHAR(150) NOT NULL, INDEX IDX_A29F1C5273B21E9C (step_id), INDEX IDX_A29F1C525FB14BA7 (level_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE home (id INT AUTO_INCREMENT NOT NULL, presentation LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE joker (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE level (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, time INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, step_id INT DEFAULT NULL, user_id INT DEFAULT NULL, title VARCHAR(200) NOT NULL, points INT NOT NULL, likes INT NOT NULL, is_validated TINYINT(1) NOT NULL, INDEX IDX_B6F7494E73B21E9C (step_id), INDEX IDX_B6F7494EA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE question_theme (question_id INT NOT NULL, theme_id INT NOT NULL, INDEX IDX_A79EF60C1E27F6BF (question_id), INDEX IDX_A79EF60C59027487 (theme_id), PRIMARY KEY(question_id, theme_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE response (id INT AUTO_INCREMENT NOT NULL, question_id INT DEFAULT NULL, response VARCHAR(150) NOT NULL, is_correct TINYINT(1) NOT NULL, INDEX IDX_3E7B0BFB1E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, coderole VARCHAR(150) NOT NULL, name VARCHAR(150) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE step (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE theme (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(150) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE app_users (id INT AUTO_INCREMENT NOT NULL, role_id INT DEFAULT NULL, username VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(200) NOT NULL, lastname VARCHAR(200) NOT NULL, email VARCHAR(200) NOT NULL, description LONGTEXT DEFAULT NULL, birthday DATETIME DEFAULT NULL, nb_games INT NOT NULL, nb_points INT NOT NULL, is_active TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_time DATETIME NOT NULL, UNIQUE INDEX UNIQ_C2502824F85E0677 (username), INDEX IDX_C2502824D60322AC (role_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_joker (user_id INT NOT NULL, joker_id INT NOT NULL, INDEX IDX_76242FCFA76ED395 (user_id), INDEX IDX_76242FCF32202C87 (joker_id), PRIMARY KEY(user_id, joker_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE coeff ADD CONSTRAINT FK_A29F1C5273B21E9C FOREIGN KEY (step_id) REFERENCES step (id)');
+        $this->addSql('ALTER TABLE coeff ADD CONSTRAINT FK_A29F1C525FB14BA7 FOREIGN KEY (level_id) REFERENCES level (id)');
+        $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494E73B21E9C FOREIGN KEY (step_id) REFERENCES step (id)');
+        $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494EA76ED395 FOREIGN KEY (user_id) REFERENCES app_users (id)');
+        $this->addSql('ALTER TABLE question_theme ADD CONSTRAINT FK_A79EF60C1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE question_theme ADD CONSTRAINT FK_A79EF60C59027487 FOREIGN KEY (theme_id) REFERENCES theme (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE response ADD CONSTRAINT FK_3E7B0BFB1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
+        $this->addSql('ALTER TABLE app_users ADD CONSTRAINT FK_C2502824D60322AC FOREIGN KEY (role_id) REFERENCES role (id)');
+        $this->addSql('ALTER TABLE user_joker ADD CONSTRAINT FK_76242FCFA76ED395 FOREIGN KEY (user_id) REFERENCES app_users (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user_joker ADD CONSTRAINT FK_76242FCF32202C87 FOREIGN KEY (joker_id) REFERENCES joker (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_joker DROP FOREIGN KEY FK_76242FCF32202C87');
+        $this->addSql('ALTER TABLE coeff DROP FOREIGN KEY FK_A29F1C525FB14BA7');
+        $this->addSql('ALTER TABLE question_theme DROP FOREIGN KEY FK_A79EF60C1E27F6BF');
+        $this->addSql('ALTER TABLE response DROP FOREIGN KEY FK_3E7B0BFB1E27F6BF');
+        $this->addSql('ALTER TABLE app_users DROP FOREIGN KEY FK_C2502824D60322AC');
+        $this->addSql('ALTER TABLE coeff DROP FOREIGN KEY FK_A29F1C5273B21E9C');
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494E73B21E9C');
+        $this->addSql('ALTER TABLE question_theme DROP FOREIGN KEY FK_A79EF60C59027487');
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494EA76ED395');
+        $this->addSql('ALTER TABLE user_joker DROP FOREIGN KEY FK_76242FCFA76ED395');
+        $this->addSql('DROP TABLE coeff');
+        $this->addSql('DROP TABLE home');
+        $this->addSql('DROP TABLE joker');
+        $this->addSql('DROP TABLE level');
+        $this->addSql('DROP TABLE question');
+        $this->addSql('DROP TABLE question_theme');
+        $this->addSql('DROP TABLE response');
+        $this->addSql('DROP TABLE role');
+        $this->addSql('DROP TABLE step');
+        $this->addSql('DROP TABLE theme');
+        $this->addSql('DROP TABLE app_users');
+        $this->addSql('DROP TABLE user_joker');
+    }
+}
